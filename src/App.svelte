d601ae87363e339462ee5aeb99b1ec197bfedb58
TanguyLichau <tanguylichau@gmail.com>
2022-12-30 13:12:16 +0100

    Cleaned up code

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,5 @@
 <script>
-  // fake {_id: '639ef90b3cb2d827312c8044', name: 'test', lastChapterRead: 10, __v: 0}
-  // pour changer une valeur $mangasListStore[0].name
-  // pour ajouter mangasListStore.update((current) => return [...current, nouvel objet])
-  // ne pas oublier de faire dockerfile pour les deux projets
   import MangaList from "./mangaList.svelte";
-
-  /*
-  if (test === true) {
-    mangasListStore.update((current) => {
-      let obj = current.find((i) => i.name === "bengbeng");
-      if (obj) {
-        obj.lastChapterRead = 696969;
-      }
-      return current;
-    });
-  }*/
 </script>
 
 <MangaList />
