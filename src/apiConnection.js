7912cb764dbaeb80929e607b4ba95d314d1a574c
TanguyLichau <tanguylichau@gmail.com>
2022-12-30 13:13:00 +0100

    Completed the api connection file

--- src/apiConnection.js
+++ src/apiConnection.js
@@ -1,7 +1,27 @@
-const API_URL = "";
+const API_URL = "http://192.168.1.54:3000";
+const USERNAME = "tanguy";
+const PASSWORD = "mdp";
 
-async function fetchJSON(response) {
+function fetchJSON(response) {
   return response.json();
+}
+
+export async function getJWT() {
+  try {
+    const response = await fetch(`${API_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: USERNAME,
+        password: PASSWORD,
+      }),
+    });
+    return fetchJSON(response);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function getAllMangas() {
@@ -14,11 +34,13 @@ export async function getAllMangas() {
 }
 
 export async function createManga(manga) {
+  const JWT_TOKEN = await getJWT();
   try {
     const response = await fetch(`${API_URL}/manga`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: "Bearer " + JWT_TOKEN.token,
       },
       body: JSON.stringify({
         name: manga.name,
@@ -31,12 +53,14 @@ export async function createManga(manga) {
   }
 }
 
-export async function updateManga(manga) {
+export async function updateMangaAPI(manga) {
+  const JWT_TOKEN = await getJWT();
   try {
     const response = await fetch(`${API_URL}/manga/${manga._id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
+        Authorization: "Bearer " + JWT_TOKEN.token,
       },
       body: JSON.stringify({
         name: manga.name,
@@ -49,10 +73,15 @@ export async function updateManga(manga) {
   }
 }
 
-export async function deleteManga(mangaId) {
+export async function deleteMangaAPI(mangaId) {
+  const JWT_TOKEN = await getJWT();
   try {
     await fetch(`${API_URL}/manga/${mangaId}`, {
       method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + JWT_TOKEN.token,
+      },
     });
   } catch (error) {
     console.error(error);
